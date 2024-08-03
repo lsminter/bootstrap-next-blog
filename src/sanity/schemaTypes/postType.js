76ef1eee2506dc601e66751cec4d4477e5a5e6ba
lsminter <lsminteris@gmail.com>
2024-08-02 18:12:50 -0700

    added sanity config

---
+++ src/sanity/schemaTypes/postType.js
@@ -0,0 +1,67 @@
+export const postType = {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'author',
+      type: 'reference',
+      to: {type: 'author'},
+    },
+    {
+      name: 'summary',
+      title: 'Summary',
+      type: 'string',
+    },
+    {
+      name: 'mainImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        }
+      ]
+    },
+    {
+      name: 'categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    },
+    {
+      name: 'publishedAt',
+      type: 'datetime',
+    },
+    {
+      name: 'body',
+      type: 'blockContent',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+}
