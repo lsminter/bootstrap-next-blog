6ce180ca641bc32b0c03f7c4bb823ac36221dbe3
lsminter <lsminteris@gmail.com>
2024-08-16 11:59:17 -0700

    updated env variable names

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,5 +1,5 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-08-02'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = process.env.SANITY_DATASET;
+export const projectId = process.env.SANITY_PROJECT_ID;
