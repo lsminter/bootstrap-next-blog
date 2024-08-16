95ca41fb2ef12698e62795ab9d3ad6fbee6ad801
lsminter <lsminteris@gmail.com>
2024-08-16 12:19:48 -0700

    added project id to client instead of .env

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
+  projectId: "0zxiq0ro", // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
   ignoreBrowserTokenWarning: true
